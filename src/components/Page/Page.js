faf973d7da79f8e75c94b8b77cbc9f76431e53d7
Tim Hofman <tim.hofman@kega.nl>
2019-08-19 22:54:12 +0200

    +: basic setup of a header component

---
+++ src/components/Page/Page.js
@@ -0,0 +1,15 @@
+import React, {Fragment} from 'react';
+
+import Header from './Header.js';
+
+class Page extends React.Component {
+    render() {
+        return (
+            <Fragment>
+                <Header />
+            </Fragment>
+        )
+    }
+}
+
+export default Page;
