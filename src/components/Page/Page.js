eab6bfe8b00c118d51215f77c4f4a4b272478ed1
Tim Hofman <tim.hofman@kega.nl>
2019-08-21 15:34:03 +0200

    +: base body component
    trying to test with props to see if you can mimic magento's
    child rendering of containers

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -1,12 +1,19 @@
 import React, {Fragment} from 'react';
 
 import Header from './Header.js';
+import Body from './Body.js';
 
 class Page extends React.Component {
     render() {
         return (
             <Fragment>
                 <Header />
+
+                <Body>
+                    <div class="item">1</div>
+                    <div class="item">2</div>
+                    <div class="item">3</div>
+                </Body>
             </Fragment>
         )
     }
