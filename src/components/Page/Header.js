9309da882e8aed7921a384e89b9c91d434323a6c
Tim Hofman <tim.hofman@kega.nl>
2019-08-23 21:08:51 +0200

    +: render products list in header
    NOTE: using new React Hooks. Though they seem to be only usable
    in a component function, not class components

--- src/components/Page/Header.js
+++ src/components/Page/Header.js
@@ -1,13 +1,57 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+import { Query } from 'react-apollo'
 
-class Header extends React.Component {
-    render() {
-        return (
-            <header>
-                <h1>Test App</h1>
-            </header>
-        )
+import gql from 'graphql-tag'
+import { useQuery } from '@apollo/react-hooks';
+
+const PRODUCTS_QUERY = gql`
+    query productsQuery {
+        products(
+            filter:{
+                price: {
+                    gt: "45"
+                }
+            }
+        ) {
+            items {
+                id,
+                name
+            }
+        }
     }
+`
+
+
+/**
+ * @doc you can use a function component when you render something
+ * without having other functions. When you components needs multiple
+ * functions, you should use a component class based on React.Component
+ *
+ * Note: you can only use React Hooks in function components
+ *
+ * @param props
+ * @returns {*}
+ * @constructor
+ */
+function Header(props) {
+
+    const { loading, error, data } = useQuery(PRODUCTS_QUERY);
+
+    if (loading) return <div>Fetching</div>
+    if (error) return <div>Error</div>
+
+    console.log(data);
+
+    return (
+        <header>
+            <h1>Test App</h1>
+            <ul>
+            {data.products.items.map((product, index) => (
+                <li key={product.id}>{product.name}</li>
+            ))}
+            </ul>
+        </header>
+    )
 }
 
 export default Header;
